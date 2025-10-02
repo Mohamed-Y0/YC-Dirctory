fb505e066208486fe665a1a49cc15934b18bad4e
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-10-02 23:36:46 +0300

    initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br /> Connect With Enterepreneusrs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submite Ideas, Vote On Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
+      </section>
+    </>
+  );
+}
