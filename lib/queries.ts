f486b4a608357f4439010131a91709f8ab32abf3
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-10-03 05:16:35 +0300

    unable experimental for PPR , and fetch the data using the params

--- lib/queries.ts
+++ lib/queries.ts
@@ -12,5 +12,24 @@ export const STARTUPS_QUERY =
     views,
     description,
     category,
-    image
+    image,
 }`);
+
+export const STARTUPS_BY_ID_QUERY = defineQuery(
+  `
+  *[_type == "startup" && _id== $id][0]{
+    _id,
+    title,
+    slug,
+    _createdAt,
+    author -> {
+        _id,name,image,bio
+    },
+    views,
+    description,
+    category,
+    image,
+      pitch
+}
+      `
+);
