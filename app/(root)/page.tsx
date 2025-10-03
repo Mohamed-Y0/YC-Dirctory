e02d168117fecf66f1fa483fa6d57546fd2fb0bd
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-10-04 01:26:03 +0300

    improve auth and make authors

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import { auth } from "@/auth";
 import SearchForm from "@/components/SearchForm";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import { STARTUPS_QUERY } from "@/lib/queries";
@@ -10,6 +11,10 @@ export default async function Home({
 }) {
   const query = (await searchParams).query;
   const params = { search: query || null };
+
+  const session = await auth();
+
+  console.log(session?.id);
 
   const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
@@ -34,8 +39,8 @@ export default async function Home({
 
         <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
-            posts.map((post: StartupTypeCard) => (
-              <StartupCard key={post?._id} post={post} />
+            posts.map(post => (
+              <StartupCard key={post?._id} post={post as StartupTypeCard} />
             ))
           ) : (
             <p className="no-results">No Startups Found</p>
--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { STARTUPS_BY_ID_QUERY } from "@/lib/queries";
+import { STARTUP_BY_ID_QUERY } from "@/lib/queries";
 import { formatDate } from "@/lib/utils";
 import { client } from "@/sanity/lib/client";
 import Image from "next/image";
@@ -16,7 +16,7 @@ export const experimental_ppr = true;
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
 
-  const post = await client.fetch(STARTUPS_BY_ID_QUERY, { id });
+  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
 
   if (!post) return notFound();
 
