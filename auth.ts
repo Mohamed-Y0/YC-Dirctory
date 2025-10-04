e0313c744070d3c225cf4c9cdff19f60c3f94b80
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-10-04 05:40:26 +0300

    fux typescript error

--- auth.ts
+++ auth.ts
@@ -9,8 +9,12 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   callbacks: {
     async signIn({
       user: { name, email, image },
-      profile: { id, login, bio },
+      profile,
     }) {
+      if (!profile) return false;
+
+      const { id, login, bio } = profile as { id: string; login: string; bio?: string };
+
       const existingUser = await client
         .withConfig({ useCdn: false })
         .fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
